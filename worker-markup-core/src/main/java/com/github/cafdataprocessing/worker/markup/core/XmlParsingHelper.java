1375e0ba55a346708648bf5253db5fbf7219fe45
Anthony McGreevy <anthony.mcgreevy@hpe.com>
2018-05-17 15:02:49 +0100

    SCMOD-3059: Updated to reflect comments

--- worker-markup-core/src/main/java/com/github/cafdataprocessing/worker/markup/core/XmlParsingHelper.java
+++ worker-markup-core/src/main/java/com/github/cafdataprocessing/worker/markup/core/XmlParsingHelper.java
@@ -15,6 +15,7 @@
  */
 package com.github.cafdataprocessing.worker.markup.core;
 
+import com.google.common.base.Strings;
 import org.jdom2.Verifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,7 +23,6 @@ import org.slf4j.LoggerFactory;
 public class XmlParsingHelper
 {
     private static final Logger LOG = LoggerFactory.getLogger(XmlParsingHelper.class);
-    private static final String ILLEGAL_START_CHARACTER_PREFIX = "ILLEGAL_START_CHARACTER_PREFIX";
 
     private XmlParsingHelper()
     {
@@ -40,40 +40,50 @@ public class XmlParsingHelper
      */
     public static String removeInvalidXmlElementNameChars(String name, String defaultElementName)
     {
-        String elementName;
-        // Check if current name is valid,
-        // if valid the current name is used, without any further processing.
+        /* 
+        * Check if current name is valid,
+        * if valid the current name is used, without any further processing.
+        * if the name is not valid it is then sanitized by removing an illegal characters
+        * if the name cannot be santized it is then discarded and the default name is returned
+        */
         if (null != Verifier.checkElementName(name)) {
-            LOG.debug("Removing invalid characters from Header Element Name: [" + name + "]");
-            char[] nameCharArray = name.toCharArray();
-            boolean startCharSet = false;
-            StringBuilder sb = new StringBuilder(name.length());
-            // Parse name for invalid character and remove invalid characters
-            for (char c : nameCharArray) {
-                if (!startCharSet && Verifier.isXMLNameStartCharacter(c)) {
-                    startCharSet = true;
-                    sb.append(c);
-                } else if (Verifier.isXMLNameCharacter(c) && c != ':') {
-                    sb.append(c);
-                }
-            }
-            // Check if elementName is empty. If empty the parsed header contained only invalid
-            // characters. In this case return the default header name, "UnreadableHeader".
-            elementName = sb.toString();
-            if (elementName.isEmpty()) {
-                elementName = defaultElementName;
-            }
-        } else {
-            elementName = name;
+            final String elementName = sanitizeElementName(name);
+            return Strings.isNullOrEmpty(elementName) ? defaultElementName : elementName;
         }
-        
-        if (elementName.matches("^[0-9].*")) {
-            elementName = ILLEGAL_START_CHARACTER_PREFIX + elementName;
-            // replace illegal characters from within the entity name incase it was missed in any previous checks
-            elementName = elementName.replace(":", "");
-        }
+        return name;
+    }
 
-        return elementName;
+    private static String sanitizeElementName(final String name)
+    {
+        LOG.debug("Removing invalid characters from Header Element Name: [" + name + "]");
+        final char[] nameCharArray = name.toCharArray();
+        final StringBuilder sb = new StringBuilder(name.length());
+
+        for (final char c : nameCharArray) {
+            if (Verifier.isXMLNameStartCharacter(c) && c != ':') {
+                LOG.debug("Identified valid start character for element name: " + c);
+                sb.append(c);
+                break;
+            }
+        }
+        // Check that an acceptable start character was found, if not return null
+        if (sb.toString().isEmpty()) {
+            return null;
+        }
+        boolean skippedStartChar = false;
+        // Parse name for invalid character and remove invalid characters
+        for (final char c : nameCharArray) {
+            //Skip the first instance of the starting character of the element name as it has already been added
+            if (String.valueOf(c).equals(sb.toString()) && !skippedStartChar) {
+                skippedStartChar = true;
+                continue;
+            }
+            // append each valid character to the element name
+            if (Verifier.isXMLNameCharacter(c) && c != ':') {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
     }
 
     /**
