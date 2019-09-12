53c8ec6e9f470fcc374ffef1eb8ca4b9f91cda56
Rahul Kulkarni <rahul.kulkarni@microfocus.com>
2019-09-12 13:08:32 -0500

    SCMOD-7512: Adding some important debug logging

--- worker-markup-core/src/main/java/com/github/cafdataprocessing/worker/markup/core/MarkupDocumentEngine.java
+++ worker-markup-core/src/main/java/com/github/cafdataprocessing/worker/markup/core/MarkupDocumentEngine.java
@@ -35,6 +35,7 @@ import com.hpe.caf.worker.markup.MarkupWorkerStatus;
 import com.hpe.caf.worker.markup.MarkupWorkerTask;
 import com.hpe.caf.worker.markup.OutputField;
 import org.jdom2.JDOMException;
+import org.jdom2.output.XMLOutputter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -190,7 +191,9 @@ public class MarkupDocumentEngine
             // Split the content into email tags and mark up the headers and body tags
             if (isEmail) {
                 emailSplitter.generateEmailTags(doc);
+                LOG.debug("Document after generateEmailTags \n" + new XMLOutputter().outputString(doc));
                 markupEngine.markUpHeadersAndBody(doc);
+                LOG.debug("Document after markUpHeadersAndBody \n" + new XMLOutputter().outputString(doc));
             }
 
             // Generate the hashes for the fields specified in the hash configuration
@@ -206,6 +209,7 @@ public class MarkupDocumentEngine
 
             // Add the list of fields to the
             workerResult.fieldList = XPathHelper.processDocumentWithXPathExpressions(doc, outputFields);
+            LOG.debug("Document after processDocumentWithXPathExpressions \n" + new XMLOutputter().outputString(doc));
 
             return workerResult;
         } catch (JDOMException jdome) {
