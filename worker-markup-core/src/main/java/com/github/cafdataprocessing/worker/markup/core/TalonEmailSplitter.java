53f4797f824f6651c1a0225828468b6eb38e7478
Rory <rory.torney@microfocus.com>
2022-10-27 14:32:13 +0100

    590014: Add test

--- worker-markup-core/src/main/java/com/github/cafdataprocessing/worker/markup/core/TalonEmailSplitter.java
+++ worker-markup-core/src/main/java/com/github/cafdataprocessing/worker/markup/core/TalonEmailSplitter.java
@@ -25,9 +25,9 @@ import java.util.regex.Pattern;
 /**
  * A Java port of <a href="https://github.com/mailgun/talon/commits/master/talon/quotations.py">quotations.py</a> from the Talon library.
  *
- * This implementation was created from the master branch of Talon on 25 October 2022.
+ * This port was created from the master branch of Talon on 25 October 2022.
  *
- * Due to this <a href="https://github.com/mailgun/talon/commit/8138ea9a604f037f47f544dfb805f13d26696275">commit</a> changing some
+ * Due to a particular <a href="https://github.com/mailgun/talon/commit/8138ea9a604f037f47f544dfb805f13d26696275">commit</a> changing some
  * behaviour that caused some of our integration tests to fail (see the
  * <a href="https://internal.almoctane.com/ui/entity-navigation?p=131002/6001&entityType=work_item&id=477072">comments on Octane ticket
  * 477072</a> for details), we are using the <a href="https://github.com/mailgun/talon/blob/v1.4.6/talon/quotations.py#L142">1.4.6</a>
@@ -45,7 +45,7 @@ import java.util.regex.Pattern;
  * Java's matcher.find() and Python's re.search( regex, input ) match any part of the string.
  * </li>
  * <li>
- * Java's matcher.lookingAt() and Python's re.match( regex, input ) match thebeginning of the string.
+ * Java's matcher.lookingAt() and Python's re.match( regex, input ) match the beginning of the string.
  * </li>
  * </ul>
  */
@@ -124,113 +124,6 @@ public final class TalonEmailSplitter
     private final static Pattern RE_HEADER = Pattern.compile(
         ": ");
 
-    public static void main(String[] args)
-    {
-        splitEmails("From: K, Tom\n"
-            + "Sent: Fri Nov 25 10:52:09 2016\n"
-            + "To: D, Mohanasundaram; S, Dan\n"
-            + "Cc: B, Rick (Rick); openvpn.community@lists.com\n"
-            + "Subject: Re: Had to reimage my ubuntu workstation.....\n"
-            + "Importance: Normal\n"
-            + "\n"
-            + "By chance was the \"green light\" ever provided for the move out of redacted.com to new.redacted.com domain?  Appreciate the help here.\n"
-            + "\n"
-            + "/tom\n"
-            + "\n"
-            + "--\n"
-            + "Tom K\n"
-            + "Strategist and Solutions\n"
-            + "EG Quality Strategy and Analytics\n"
-            + "\n"
-            + "\n"
-            + "On 10/4/16, 4:30 PM, \"openvpn.community-request@lists.com on behalf of D, Mohanasundaram\" <openvpn.community-request@lists.com on behalf of mohanasundaram.d@hpe.com> wrote:\n"
-            + "\n"
-            + "    Thanks Dan,\n"
-            + "\n"
-            + "    Hope we take action soon to move out of redacted.com to new.redacted.com domain. This\n"
-            + "    may stop working on Oct 31 or some day. Who are the OpenVPM admins?\n"
-            + "\n"
-            + "    Thanks\n"
-            + "    Mohan\n"
-            + "\n"
-            + "\n"
-            + "\n"
-            + "    On Mon, 2016-10-03 at 09:45 -0600, S, Dan wrote:\n"
-            + "    > Mohan,\n"
-            + "    >\n"
-            + "    > We have not yet migrated the osprey[01].fc.com systems.  These can be moved at any time to osprey[01].ftc.net but I need the \"green light\" from the OpenVPN admins to do so -- earlier in the year we decided to wait.\n"
-            + "    >\n"
-            + "    > As for osprey[01].linux.com, these also remain intact, but there are new hosts osprey[01].osp.com that are set up in DNS.  They don't appear to respond to any ping or SSH attempts.\n"
-            + "    >\n"
-            + "    >\n"
-            + "    > Dan S\n"
-            + "    > Hewlett Packard Enterprise Open Source Program Office\n"
-            + "    >\n"
-            + "    > -----Original Message-----\n"
-            + "    > From: openvpn.community-request@lists.com [mailto:openvpn.community-request@lists.com] On Behalf Of D, Mohanasundaram\n"
-            + "    > Sent: Monday, October 03, 2016 8:25 AM\n"
-            + "    > To: B, Rick (Rick) <rick.b@hpe.com>\n"
-            + "    > Cc: openvpn.community@lists.com\n"
-            + "    > Subject: Re: Had to reimage my ubuntu workstation.....\n"
-            + "    >\n"
-            + "    > Looks like we need to find the equivalents for the following hp.com servers and replace them at https://hpedia.osp.com/wiki/Open\n"
-            + "    > ospra0.linux.hp.com\n"
-            + "    > ospra1.linux.hp.com\n"
-            + "    > http://autocache.com/\n"
-            + "    > http://remoteaccess.com\n"
-            + "    > intranet.com\n"
-            + "    > http://itsupport.com/portal/site/documentdetail?docid=KM32\n"
-            + "    > vwpc.austin.com\n"
-            + "    >\n"
-            + "    > I do not know the new servers and looking for them. If some one has please do replace.\n"
-            + "    >\n"
-            + "    > Regards\n"
-            + "    > Mohan\n"
-            + "    >\n"
-            + "    >\n"
-            + "    > On Mon, 2016-10-03 at 08:36 -0400, Rick B wrote:\n"
-            + "    > > This tends to be the most up-to-date resource for information:\n"
-            + "    > >\n"
-            + "    > > https://hpedia.osp.com/wiki/Open\n"
-            + "    > >\n"
-            + "    > > If you find something different,  please update the wiki.\n"
-            + "    > >\n"
-            + "    > > I use both openvpn on 16.76 as well as openconnect with the juniper\n"
-            + "    > > gateways:\n"
-            + "    > >\n"
-            + "    > > https://hpedia.osp.com/wiki/OpenConnect\n"
-            + "    > >\n"
-            + "    > > I find openconnect to have somewhat better latency but it does seem\n"
-            + "    > > susceptible to 'noise' that causes connections to occasionally drop.\n"
-            + "    > >\n"
-            + "    > > Rick\n"
-            + "    > >\n"
-            + "    > >\n"
-            + "    > >\n"
-            + "    > > On 10/02/2016 05:07 PM, L, Benson wrote:\n"
-            + "    > > > Mike,\n"
-            + "    > > >\n"
-            + "    > > >\n"
-            + "    > > >\n"
-            + "    > > > Which certificate are you referring to?  Is this the Class B digital\n"
-            + "    > > > badge certificate or is it the SSL Certificate used for servers?\n"
-            + "    > > > Both have been migrated to http://mydigi.com/ now.\n"
-            + "    > > >\n"
-            + "    > > >\n"
-            + "    > > >\n"
-            + "    > > > Thank you!\n"
-            + "    > > >\n"
-            + "    > > >\n"
-            + "    > > >\n"
-            + "    > > > Benson\n"
-            + "    > > >\n"
-            + "    > > >\n"
-            + "    > > >\n"
-            + "    > >\n"
-            + "    >\n"
-            + "    >");
-    }
-
     /**
      * Given a message(which may consist of an email conversation thread with multiple emails), mark the lines to identify split lines,
      * content lines and empty lines.
@@ -274,7 +167,7 @@ public final class TalonEmailSplitter
 
         final StringBuffer stringBuffer = new StringBuffer();
         final Matcher matcher = RE_LINK.matcher(msgBody);
-        while (matcher.find()) { // TODO check
+        while (matcher.find()) {
             final int newlineIndex = msgBody.substring(0, matcher.start()).lastIndexOf("\n");
             if (msgBody.charAt(newlineIndex + 1) == '>') {
                 matcher.appendReplacement(stringBuffer, matcher.group());
@@ -293,7 +186,7 @@ public final class TalonEmailSplitter
         //
         // This ensures headers can be identified if they are indented with spaces
         for (int i = 0; i < lines.length; i++) {
-            lines[i] = CharMatcher.whitespace().trimLeadingFrom(lines[i]); // TODO check
+            lines[i] = CharMatcher.whitespace().trimLeadingFrom(lines[i]);
         }
 
         return markMessageLines(lines);
